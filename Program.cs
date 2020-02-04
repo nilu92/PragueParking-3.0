2beab83ab45484361ea3dd3e53e55eed0198875c
nilu92 <57625806+nilu92@users.noreply.github.com>
2020-02-04 18:26:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PragueParking_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        
+        }
+   
+        public void Run() 
+        {
+        
+        }
+    
+    }
+}
