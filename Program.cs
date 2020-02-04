fc6d62ce6e3c0f2b11fe48a5af74b90782c81f18
nilu92 <57625806+nilu92@users.noreply.github.com>
2020-02-04 21:09:25 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,11 +13,42 @@ namespace PragueParking_3._0
         static void Main(string[] args)
         {
             new Program().Run();
-        
+           
         }
    
         public void Run() 
         {
+            
+        }
+    
+        public void AddVehicle() 
+        {
+
+            SqlConnection cn = new SqlConnection(@"server = DESKTOP-E57017B\SQLEXPRESS; Database=Parking; Integrated Security=true");
+            cn.Open();
+            int id, parkspot;
+            string typeOfVehicle, regnumb;
+            Console.WriteLine("Enter id and parkspot");
+            id = int.Parse(Console.ReadLine());
+            parkspot = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter type and regnumb");
+            typeOfVehicle = Console.ReadLine();
+            //Add function that checks if regnumb exist in table
+            regnumb = Console.ReadLine();
+            
+            SqlCommand cmd = new SqlCommand("insert into fordon values('" + id + "','" + typeOfVehicle + "','" + regnumb + "','" + parkspot + "')", cn);
+            int i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                Console.WriteLine("Insertion successful!");
+            }
+            cn.Close();
+
+        }
+   
+        public void CheckForDuplicates() 
+        {
+            
         
         }
     
